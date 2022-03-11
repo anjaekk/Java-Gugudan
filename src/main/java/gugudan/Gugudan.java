853e9c3d2b1d97ea57cd3d1a56a63cd9ac3947b4
anjaekk <anjaekk@gmail.com>
2022-03-11 23:38:18 +0900

    Add: gugudan 5

--- src/main/java/gugudan/Gugudan.java
+++ src/main/java/gugudan/Gugudan.java
@@ -1,30 +1,34 @@
 package gugudan;
 
+import java.util.Scanner;
+
 public class Gugudan {
 
     public static void main(String[] args) {
         // 2단
-//        System.out.println("2단");
-//        System.out.println(2 * 1);
-//        System.out.println(2 * 2);
-//        System.out.println(2 * 3);
-//        System.out.println(2 * 4);
-//        System.out.println(2 * 5);
-//        System.out.println(2 * 6);
-//        System.out.println(2 * 7);
-//        System.out.println(2 * 8);
-//        System.out.println(2 * 9);
+        System.out.println("2단");
+        System.out.println(2 * 1);
+        System.out.println(2 * 2);
+        System.out.println(2 * 3);
+        System.out.println(2 * 4);
+        System.out.println(2 * 5);
+        System.out.println(2 * 6);
+        System.out.println(2 * 7);
+        System.out.println(2 * 8);
+        System.out.println(2 * 9);
+
         // 3단
-//        System.out.println("3단");
-//        System.out.println(3 * 1);
-//        System.out.println(3 * 2);
-//        System.out.println(3 * 3);
-//        System.out.println(3 * 4);
-//        System.out.println(3 * 5);
-//        System.out.println(3 * 6);
-//        System.out.println(3 * 7);
-//        System.out.println(3 * 8);
-//        System.out.println(3 * 9);
+        System.out.println("3단");
+        System.out.println(3 * 1);
+        System.out.println(3 * 2);
+        System.out.println(3 * 3);
+        System.out.println(3 * 4);
+        System.out.println(3 * 5);
+        System.out.println(3 * 6);
+        System.out.println(3 * 7);
+        System.out.println(3 * 8);
+        System.out.println(3 * 9);
+
         // 4단
         System.out.println("4단");
         int result = 4 * 1;
@@ -45,7 +49,22 @@ public class Gugudan {
         System.out.println(result);
         result = 4 * 9;
         System.out.println(result);
-        // 5단
 
+        // 5단
+        System.out.println("출력할 단은?");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number * 1);
+        System.out.println(number * 2);
+        System.out.println(number * 3);
+        System.out.println(number * 4);
+        System.out.println(number * 5);
+        System.out.println(number * 6);
+        System.out.println(number * 7);
+        System.out.println(number * 8);
+        System.out.println(number * 9);
+
+        // 6단
+        
     }
 }
