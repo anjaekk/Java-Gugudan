570c93d363c19cab374e0d5557a7179858d079ff
anjaekk <anjaekk@gmail.com>
2022-03-20 23:09:31 +0900

    Add: gugudan 6,7 iteration

--- src/main/java/gugudan/Gugudan.java
+++ src/main/java/gugudan/Gugudan.java
@@ -64,7 +64,19 @@ public class Gugudan {
         System.out.println(number * 8);
         System.out.println(number * 9);
 
-        // 6단
-        
+        // 6단 - While
+        System.out.println("6단");
+        int i = 1;
+        while(i < 10) {
+            System.out.println(6 * i);
+            i = i + 1;
+        }
+
+        // 7단 - for
+        System.out.println("7단");
+        for(int j = 1; j < 10; j++) {
+            System.out.println(7 * j);
+        }
+
     }
 }
